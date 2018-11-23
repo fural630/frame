4e04c2c262793d9959b599be049ea3e240d3af7e
fural630 <312651213@qq.com>
2018-11-23 14:55:14 +0800

    code

--- frame-admin/src/main/java/com/frame/CodeGenerator.java
+++ frame-admin/src/main/java/com/frame/CodeGenerator.java
@@ -2,11 +2,11 @@ package com.frame;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.ResourceBundle;
 
-import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import org.apache.commons.lang3.StringUtils;
+
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
@@ -19,55 +19,93 @@ import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
-import com.sun.management.GarbageCollectorMXBean;
-import com.sun.org.apache.xerces.internal.impl.dv.xs.DecimalDV;
 
 public class CodeGenerator {
-	
-	public static String scanner(String tip) {
-		Scanner scanner = new Scanner(System.in);
-		StringBuilder help = new StringBuilder();
-		help.append("请输入" + tip + "：");
-		System.out.println(help.toString());
-		if (scanner.hasNext()) {
-			String ipt = scanner.next();
-			if (StringUtils.isNotEmpty(ipt)) {
-				return ipt;
-			}
-		}
-		throw new MybatisPlusException("请输入正确的" + tip + "！");
-	}
 
 	public static void main(String[] args) {
 		// 代码生成器
 		AutoGenerator mpg = new AutoGenerator();
 
+		ResourceBundle rb = ResourceBundle.getBundle("codeGenerator");
+		String author = rb.getString("author");
+		boolean fileOverride = Boolean.parseBoolean(rb.getString("fileOverride"));
+		boolean open = Boolean.parseBoolean(rb.getString("open"));
+		boolean baseResultMap = Boolean.parseBoolean(rb.getString("baseResultMap"));
+		boolean baseColumnList = Boolean.parseBoolean(rb.getString("baseColumnList"));
+		String entityName = rb.getString("entityName");
+		String mapperName = rb.getString("mapperName");
+		String xmlName = rb.getString("xmlName");
+		String serviceName = rb.getString("serviceName");
+		String serviceImplName = rb.getString("serviceImplName");
+		String controllerName = rb.getString("controllerName");
+		
+		
+		String url = rb.getString("url");
+		String driverName = rb.getString("driverName");
+		String username = rb.getString("username");
+		String password = rb.getString("password");
+		
+		String parent = rb.getString("parent");
+		String moduleName = rb.getString("moduleName");
+		String entity = rb.getString("entity");
+		String service = rb.getString("service");
+		String serviceImpl = rb.getString("serviceImpl");
+		String mapper = rb.getString("mapper");
+		String xml = rb.getString("xml");
+		String controller = rb.getString("controller");
+
 		// 全局配置
 		GlobalConfig gc = new GlobalConfig();
 		String projectPath = System.getProperty("user.dir");
 		gc.setOutputDir(projectPath + "/src/main/java");
-		gc.setAuthor("zhangzm");
-		gc.setOpen(false);
+		gc.setFileOverride(fileOverride);
+		gc.setAuthor(author);
+		gc.setOpen(open);
 		gc.setDateType(DateType.ONLY_DATE);
-		gc.setBaseColumnList(true);
-		gc.setBaseResultMap(true);
+		gc.setBaseColumnList(baseColumnList);
+		gc.setBaseResultMap(baseResultMap);
+		if (StringUtils.isNoneBlank(entityName)) {
+			gc.setEntityName(entityName);
+		}
+		if (StringUtils.isNoneBlank(mapperName)) {
+			gc.setEntityName(mapperName);
+		}
+		if (StringUtils.isNoneBlank(xmlName)) {
+			gc.setEntityName(xmlName);
+		}
+		if (StringUtils.isNoneBlank(serviceName)) {
+			gc.setEntityName(serviceName);
+		}
+		if (StringUtils.isNoneBlank(serviceImplName)) {
+			gc.setEntityName(serviceImplName);
+		}
+		if (StringUtils.isNoneBlank(controllerName)) {
+			gc.setEntityName(controllerName);
+		}
 		mpg.setGlobalConfig(gc);
 
 		// 数据源配置
 		DataSourceConfig dsc = new DataSourceConfig();
-		dsc.setUrl("jdbc:mysql://localhost:3306/demo?useUnicode=true&characterEncoding=UTF-8");
-//		dsc.setUrl("jdbc:oracle:thin:@172.16.5.11:1521:orcl");
-		// dsc.setSchemaName("public");
-		dsc.setDriverName("com.mysql.jdbc.Driver");
-//		dsc.setDriverName("oracle.jdbc.driver.OracleDriver");
-		dsc.setUsername("root");
-		dsc.setPassword("root");
+		dsc.setUrl(url);
+		dsc.setDriverName(driverName);
+		dsc.setUsername(username);
+		dsc.setPassword(password);
+		
 		mpg.setDataSource(dsc);
 
 		// 包配置
 		PackageConfig pc = new PackageConfig();
-		pc.setModuleName("sys");
-		pc.setParent("com.frame.web.module");
+		System.out.println(moduleName);
+		System.out.println(parent);
+		pc.setModuleName(moduleName);
+        pc.setParent(parent);
+        pc.setEntity(entity);
+        pc.setService(service);
+        pc.setServiceImpl(serviceImpl);
+        pc.setMapper(mapper);
+        pc.setXml(xml);
+        pc.setController(controller);
+		
 		mpg.setPackageInfo(pc);
 
 		// 自定义配置
@@ -94,11 +132,11 @@ public class CodeGenerator {
 		StrategyConfig strategy = new StrategyConfig();
 		strategy.setNaming(NamingStrategy.underline_to_camel);
 		strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-//		strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
-//		strategy.setEntityLombokModel(true);
+		// strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+		// strategy.setEntityLombokModel(true);
 		strategy.setRestControllerStyle(true);
-//		strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
-		strategy.setInclude("sys_demo");
+		// strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+		strategy.setInclude("sys_test");
 		strategy.setSuperEntityColumns("id");
 		strategy.setControllerMappingHyphenStyle(true);
 		strategy.setTablePrefix("sys_");
