07a3d85aee2217b5c71d8707cc46fa8308f99983
oracle123 <312651213@qq.com>
2018-12-09 21:49:34 +0800

    1

--- frame-admin/src/main/resources/static/js/user.js
+++ frame-admin/src/main/resources/static/js/user.js
@@ -18,6 +18,11 @@ var vm = new Vue({
 		  { text : '正常', value : '1' },
 		  { text : '禁用', value : '0' }
 	  ],
+	  ruleValidate : {
+		userName : [
+			{required: true, message: '用户名不能为空', trigger: 'blur'}
+		],
+	  },
 	  showList : true,
 	  title : ''
   },
@@ -36,8 +41,16 @@ var vm = new Vue({
 		  this.showList = false;
 		  this.user.status = 1;
 	  },
+	  saveOrUpdate : function () {
+		  alert(1123);
+	  },
 	  reload : function () {
 		  this.showList = true;
+	  },
+	  handleSubmit : function (name) {
+		  handleSubmitValidate(vm, name, function () {
+              vm.saveOrUpdate();
+          });
 	  },
       handleReset: function (name) {
     	  this.$refs[name].resetFields();
@@ -53,30 +66,28 @@ layui.use('table', function() {
 	table.render({
 		elem: '#userTable'
 		,toolbar: '#userToolbar'
-		,url:'/sys/user/list'
+		,url:'/sys/user/page'
 		,autoSort : false
-		,request : {
-			pageName : 'nowPage',
-			limitName : 'pageSize'
-		}
-		,response : {
-			countName : 'totalNum',
-			dataName : 'items',
-			statusCode : 200
+		,parseData: function(res){
+		    return {
+		      "code": res.code,
+		      "count": res.page.total,
+		      "data": res.page.list
+		    };
 		}
 		,cols: [[
 		  {type:'checkbox'}
-		  ,{field : 'id', width : 60, title : 'ID', sort : true }
+		  ,{field : 'id',width : 60, title : 'ID', sort : true }
 		  ,{field : 'name', width : 80, title : '姓名'}
 		  ,{field : 'account', width : 200, title : '账号'}
 		  ,{field : 'phone', width : 140, title : '电话'}
 		  ,{field : 'email', width : 180, title : '邮箱'}
-		  ,{field : 'sex', width : 70,  title : '性别', sort : true, 
+		  ,{field : 'sex', width : 70,  title : '性别',
 			  templet : function (d) {
 				  return transGender(d.sex)
 			  }
 		  }
-		  ,{field : 'birthDay', width : 120, title : '出生日期', 
+		  ,{field : 'birthDay', width : 140, title : '出生日期', sort : true,
 			  templet : function (d) {
 				  return transDate(d.birthDay, 'yyyy年MM月dd日')
 			  }
@@ -92,10 +103,14 @@ layui.use('table', function() {
 	});
 	
 	table.on('sort(userTable)', function (obj){
+		var column = obj.field;
+		if (obj.field == "birthDay") {
+			column = "birth_day";
+		}
 		table.reload('userTable', {
 		  initSort : obj,
 		  where : {
-			  field: obj.field,
+			  field: column,
 			  order: obj.type
 		  }
 	  });
